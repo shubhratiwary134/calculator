bb6ff47c9741f14a1f2d228977e9d6d76d466059
shubhra tiwary <shubhratiwary5@gmail.com>
2025-06-20 03:48:48 +0530

    rest of the functions added

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -8,8 +8,20 @@ contract Calculator {
     // view keyword means that we are only reading and not writing data from the blockchain .
     // pure keyword means that the function does not read or write from the blockchain it uses the data given to it.
     // pure keyword is for when we use only the argument to perform operations
-    function add(uint num1, uint num2) public pure returns (uint) {
+    function add(uint num1, uint num2) external pure returns (uint) {
         uint sum = num1 + num2;
         return sum;
+    }
+    function subtract(uint num1, uint num2) external pure returns (uint) {
+        uint difference = num1 - num2;
+        return difference;
+    }
+    function multiply(uint num1, uint num2) external pure returns (uint) {
+        uint answer = num1 * num2;
+        return answer;
+    }
+    function division(uint num1, uint num2) external pure returns (uint) {
+        uint answer = num1 / num2;
+        return answer;
     }
 }
