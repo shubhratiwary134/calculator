129f6cbb874e38d905cd2e891b7ecf4f505fb74d
shubhra tiwary <shubhratiwary5@gmail.com>
2025-06-20 03:01:43 +0530

    variable created

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 contract Calculator {
-    address 
+    // the public keyword here means that the state variable can be accessed by outside
+    // public keyword here automatically creates a getter function for the variable
+    uint256 public result;
 }
